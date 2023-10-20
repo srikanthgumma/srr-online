926d26ed9339f0c2c16108c36936fef232f708d0
srikanthgumma <srikanthgummadi516@gmail.com>
2023-10-20 18:37:36 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <marquee>    <h1>Venu Nani Jewellery Shop Online -- COMING SOON</h1>
+</marquee>
+    <br></br>
+    <br></br>
+    <br></br>
+
+    <div  style={{display:"grid", gridTemplateColumns:"300px 300px 300px 300px", gap:"10px",marginBottom:"30px", width:"100%", justifyContent:"space-evenly"}}>
+      <div id="kk" >Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px red", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px pink", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px blue", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px orange", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px yellow", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px brown", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px navy", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px black", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px black", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px black", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px black", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+    </div>
+    <footer style={{padding:"90px", backgroundColor:"black", color:"white"}}>
+      Copyright Details Here
+    </footer>
+    </div>
+
+  );
+}
+
+export default App;
