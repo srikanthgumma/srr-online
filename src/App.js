57dab1e456a7f0bc87e3b3462495a4eb1a3f0a5a
srikanthgumma <srikanthgummadi516@gmail.com>
2023-10-21 11:36:35 +0530

    gre

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import pic1 from './media/srr1.jpeg';
+import pic2 from './media/srr2.jpeg';
+import pic3 from './media/srr3.jpeg';
 
 function App() {
   return (
     <div className="App">
       <marquee>    <h1>Venu Nani Jewellery Shop Online -- COMING SOON</h1>
 </marquee>
-    <br></br>
-    <br></br>
-    <br></br>
-
-
-    <div  style={{display:"grid", gridTemplateColumns:"300px 300px 300px", gap:"10px",marginBottom:"30px", width:"100%", justifyContent:"space-evenly"}}>
-      <div id="kk" >Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px red", textAlign:"center", lineHeight:"300px"}}><img src='' /></div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px pink", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px blue", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px orange", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px yellow", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px brown", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px navy", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px black", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px black", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px black", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px black", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
-    </div>
-    <footer style={{padding:"90px", backgroundColor:"black", color:"white"}}>
+<div id='first'>
+  <img  src={pic1} />
+  <img  src={pic3} />
+  <img  src={pic2} />
+  <img  src={pic1} />
+  <img  src={pic2} />
+  <img  src={pic3} />
+  <img  src={pic1} />
+  <img  src={pic2} />
+  <img  src={pic3} />
+</div>
+    <footer id="foot" >
       Copyright Details Here
     </footer>
     </div>
