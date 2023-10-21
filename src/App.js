18672d84c60d6dc7685bc52f3843cb8d1a9bee66
srikanthgumma <srikanthgummadi516@gmail.com>
2023-10-21 11:03:59 +0530

    d

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
     <div  style={{display:"grid", gridTemplateColumns:"300px 300px 300px", gap:"10px",marginBottom:"30px", width:"100%", justifyContent:"space-evenly"}}>
       <div id="kk" >Dont worry. images coming soon</div>
-      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px red", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
+      <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px red", textAlign:"center", lineHeight:"300px"}}><img src='' /></div>
       <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px pink", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
       <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px blue", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
       <div style={{width:"250px",height:"300px", backgroundColor:"white", boxShadow:"0 0 20px orange", textAlign:"center", lineHeight:"300px"}}>Dont worry. images coming soon</div>
